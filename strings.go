41f5aec3970c56986d05f37d82d898c0421a24a3
Michael Matranga <michael.matranga@stratisiot.com>
2019-09-05 13:40:59 -0400

    Skipping vendor in strings.

--- strings.go
+++ strings.go
@@ -11,11 +11,15 @@ import (
 	"os"
 	"path/filepath"
 	"sort"
+	"strings"
 	"strconv"
 )
 
 func ObfuscateStrings(gopath string) error {
 	return filepath.Walk(gopath, func(path string, info os.FileInfo, err error) error {
+		if strings.Contains(path, "vendor") {
+			return nil
+		}
 		if err != nil {
 			return err
 		}
